c92d5056b4c87b6aae71a2dc19307de9b8b90f3f
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-05 12:59:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+import makeWASocket, {
+  fetchLatestBaileysVersion,
+  useMultiFileAuthState,
+  DisconnectReason
+} from "@whiskeysockets/baileys";
+import qrcode from "qrcode-terminal";
+import { Admincontrols } from "./src/admin/controlAdmin.js";
+import dotenv from "dotenv";
+dotenv.config();
+const start = async () => {
+  const { state, saveCreds } = await useMultiFileAuthState("./auth_save");
+  const { version } = await fetchLatestBaileysVersion();
+
+  const sock = makeWASocket({
+    version,
+    auth: state,
+    printQRInTerminal: false
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("connection.update", ({ connection, lastDisconnect, qr }) => {
+    try {
+      if (qr) {
+        qrcode.generate(qr, { small: true });
+        console.log("Scan QR untuk login WhatsApp");
+      }
+
+      if (connection === "close") {
+        const reason = lastDisconnect?.error?.output?.statusCode;
+        const shouldReconnect = reason !== DisconnectReason.loggedOut;
+
+        console.log("Koneksi terputus.");
+        console.log("Reconnect?", shouldReconnect);
+
+        if (shouldReconnect) {
+          console.log("Reconnect dalam 5 detik...");
+          setTimeout(start, 5000);
+        } else {
+          console.log(" Logout permanen. Hapus folder auth_save untuk login ulang.");
+        }
+      }
+
+      if (connection === "open") {
+        console.log(" Bot WhatsApp berhasil terhubung!");
+      }
+    } catch (err) {
+      console.error("⚠️ Error:", err);
+      setTimeout(start, 5000);
+    }
+  });
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const msg = messages[0]
+    try {
+      const chatId = msg.key.remoteJid;
+      const text = msg.message.conversation || msg.message.extendedTextMessage?.text || "";
+      if (!chatId?.endsWith("@g.us")) return;
+      Admincontrols(sock, chatId, msg, text);
+    } catch (err) {
+      console.log(err)
+    }
+  })
+};
+
+start();
---
+++ src/admin/controlAdmin.js
@@ -0,0 +1,42 @@
+export const Admincontrols = async (sock, chatId, msg, text) => {
+  try {
+    console.log(msg.key.participant)
+
+    const metadata = await sock.groupMetadata(chatId);
+    const admin = metadata.participants
+      .filter(p => p.admin)
+      .map(p => ({
+        jid: p.id,
+        pn: p.pn,
+        role: p.admin
+      }));
+    const botId = "179573169848377@lid"
+    console.log(botId)
+    const isAdmin = admin.some(a => a.jid === msg.key.participant)
+    const isBotadmin = admin.some(a => a.jid === botId)
+    //cmd
+    if (text.startsWith("!addnews")) {
+      if (!isAdmin) return sock.sendMessage(chatId, { text: "admin only" }, { quoted: msg });
+      sock.sendMessage(chatId, { text: "fitur addnews" }, { quoted: msg })
+    }
+    if (text === "!close") {
+      if (!isBotadmin) return sock.sendMessage(chatId, { text: "bot tidak diberikan akses admin\njadikan bot sebagai admin grub untuk menggunakan cmd ini" }, { quoted: msg })
+      if (!isAdmin) return sock.sendMessage(chatId, { text: "admin only" }, { quoted: msg });
+      await sock.groupSettingUpdate(chatId, 'announcement');
+    }
+    if (text === "!open") {
+      if (!isBotadmin) return sock.sendMessage(chatId, { text: "bot tidak diberikan akses admin\njadikan bot sebagai admin grub untuk menggunakan cmd ini" }, { quoted: msg })
+      if (!isAdmin) return sock.sendMessage(chatId, { text: "admin only" }, { quoted: msg });
+      await sock.groupSettingUpdate(chatId, 'not_announcement')
+    }
+    if (text.startsWith("!kick")) {
+      if (!isBotadmin) return sock.sendMessage(chatId, { text: "bot tidak diberikan akses admin\njadikan bot sebagai admin grub untuk menggunakan cmd ini" }, { quoted: msg })
+      if (!isAdmin) return sock.sendMessage(chatId, { text: "admin only" }, { quoted: msg });
+      const mentions = msg.message.extendedTextMessage?.contextInfo?.mentionedJid || [];
+      if (!mentions.length) return sock.sendMessage(chatId, { text: "tag target yang akan di kick" }, { quoted: msg });
+      await sock.groupParticipantsUpdate(chatId, mentions, 'remove')
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
