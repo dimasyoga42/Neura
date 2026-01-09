873caf6f850d091356e6d5fd2d4e12b74251ead5
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-09 15:27:53 +0700

    cmd any

--- src/modul/cmdControls.js
+++ src/modul/cmdControls.js
@@ -106,7 +106,7 @@ export const cmdMenucontrol = async (sock, chatId, msg, text) => {
     if (isBan(sock, chatId, msg)) return;
     hidetag(sock, chatId, msg, text);
   }
-  if (text.startsWith("!stiker") || msg.message.imageMessage?.caption === "!stiker") {
+  if (text.startsWith("!stiker") || msg.message.imageMessage?.caption === "!s") {
     if (isBan(sock, chatId, msg)) return;
     sticker(sock, msg, chatId);
   }
