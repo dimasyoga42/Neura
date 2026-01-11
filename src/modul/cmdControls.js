6047fe62e1ddb933156c1bf81f1697d5724d372b
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-11 14:33:52 +0700

    add deks

--- src/modul/cmdControls.js
+++ src/modul/cmdControls.js
@@ -20,7 +20,7 @@ import Bossdef from "../plugins/toram/bos.js";
 import { dyePredictor } from "../plugins/toram/dye.js";
 import { leveling } from "../plugins/toram/lv.js";
 import { clearRaid, createRaid, joinRaid, leaveRaid, viewRaid } from "../plugins/toram/raidControl.js";
-import tanaka, { formatResultMessage, parseCommand } from "../plugins/toram/tanaka.js";
+import { formatResultMessage, parseCommand, tanaka } from "../plugins/toram/tanaka.js";
 import { downloadToMp3 } from "../plugins/vip/downloader/music.js";
 import fs from "fs";
 export const cmdMenucontrol = async (sock, chatId, msg, text) => {
