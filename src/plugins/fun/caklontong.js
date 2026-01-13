0b6f5467e9393cb7f160946a6dd7e9fd4541f045
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-13 10:06:34 +0700

    add game caklontong

--- src/plugins/fun/caklontong.js
+++ src/plugins/fun/caklontong.js
@@ -1,24 +1,26 @@
 import axios from "axios"
 
-const answer = []
+const answer = new Map();
 
 export const Caklontong = async (sock, chatId, msg, text) => {
   try {
-    if (answer.id === chatId) return sock.sendMessage(chatId, { text: "selesaikan pertanyaan sebelumnya" }, { quoted: msg });
+    if (answer.has(chatId)) return sock.sendMessage(chatId, { text: "selesaikan pertanyaan sebelumnya" }, { quoted: msg });
     const res = await axios.get("https://api.deline.web.id/game/caklontong")
     const data = await res.data.data
     const messageData = `*caklontong:*
-    - ${data.soal}
-    > gunakan !j untuk menjawab
+    - ${data.soal}\nwaktu: 60detik\n> gunakan !j untuk menjawab
       `.trim()
-    sock.sendMessage(chatId, { text: messageData }, { quoted: msg })
-    const newAnswer = {
-      id: chatId,
-      jawaban: data.jawaban
-    }
-    answer.push(newAnswer);
-    console.log(answer)
+    sock.sendMessage(chatId, { text: messageData }, { quoted: msg });
+    answer.set(chatId, {
+      jawaban: data.jawaban,
+      timestamp: Date.now()
+    });
+    setTimeout(() => {
+      if (answer.has(chatId)) {
+        answer.delete(chatId);
+        sock.sendMessage(chatId, { text: `waktu habis,jawaban yang benar adalah:${data.jawaban}` }, { quoted: msg })
+      }
+    }, 60000)
   } catch (err) {
-
   }
 }
