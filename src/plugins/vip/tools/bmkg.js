8e22083492b06d60cec2a1b5aaddc450ec8b3650
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-15 10:08:46 +0700

    update feat gempa

--- src/plugins/vip/tools/bmkg.js
+++ src/plugins/vip/tools/bmkg.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const autoGempa = async (sock, chatId, msg) => {
   try {
-    const baseURL = "https://static.bmkg.go.id"
     const res = await axios.get("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json");
     const data = res.data.Infogempa
 
@@ -18,7 +17,7 @@ kedalaman: ${data.gempa.Kedalaman}
 wilayah: ${data.gempa.Wilayah}
 potensi: ${data.gempa.Potensi}
     `.trim()
-    sock.sendMessage(chatId, { image: { url: `${baseURL}/${data.gempa.shakemap}` }, caption: messagetxt }, { quoted: msg })
+    sock.sendMessage(chatId, { image: { url: `https://static.bmkg.go.id/${data.gempa.Shakemap}` }, caption: messagetxt }, { quoted: msg })
   } catch (err) {
     console.log(err.message);
   }
