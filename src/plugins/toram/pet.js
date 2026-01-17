b4a424d0fe623cf17ff35078e14cf969301294f7
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-17 21:40:04 +0700

    update fitur pet

--- src/plugins/toram/pet.js
+++ src/plugins/toram/pet.js
@@ -3,7 +3,7 @@ import { petGuide } from "../../config/variabel.js";
 export const pet = (sock, chatId, msg, text) => {
   try {
     const arg = text.replace("!pet", "").trim();
-    if (!arg) return sock.sendMessage(chatId, { text: "format anda salah\ngunakan !pet pilihan\npilihan yang tersedia:\nleveling\nskill\npadu\nlatih\n> data masih dalam proses update jika ada kesalahan mohon !report" }, { quoted: msg });
+    if (!arg) return sock.sendMessage(chatId, { text: "format anda salah\ngunakan !pet pilihan\npilihan yang tersedia:\nleveling\nskill\nmaxlv\nlatih\n> data masih dalam proses update jika ada kesalahan mohon !report" }, { quoted: msg });
     if (arg === "leveling") {
       sock.sendMessage(chatId, { image: { url: 'https://i.ibb.co/dsr6H0t7/Pet-Nature.jpg' } }, { quoted: msg })
       sock.sendMessage(chatId, { text: petGuide.levelingPet }, { quoted: msg })
@@ -11,7 +11,7 @@ export const pet = (sock, chatId, msg, text) => {
       sock.sendMessage(chatId, { text: petGuide.skillPet }, { quoted: msg })
     } else if (arg === "latih") {
       sock.sendMessage(chatId, { text: petGuide.trainingPet }, { quoted: msg })
-    } else if (arg === "padu") {
+    } else if (arg === "maxlv") {
       sock.sendMessage(chatId, { image: { url: `https://i.ibb.co/B245jnXN/Fuse-Diagram.jpg` }, caption: petGuide.fusionPet }, { quoted: msg })
     } else {
       sock.sendMessage(chatId, { text: "pilihan anda tidak ada" }, { quoted: msg })
