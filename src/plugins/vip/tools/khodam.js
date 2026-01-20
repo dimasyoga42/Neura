b247a3378bb227c8414faee02ce1d602bc89dc8e
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-20 13:56:55 +0700

    fitur kodam

--- src/plugins/vip/tools/khodam.js
+++ src/plugins/vip/tools/khodam.js
@@ -4,7 +4,11 @@ import axios from "axios"
 export const khodam = async (sock, chatId, msg) => {
   try {
     const data = await axios.get(`https://api.neoxr.eu/api/khodam?apikey=${process.env.NEOXR_KEY}`)
-    const res = data.data
+    const res = data.data.data
+
+    const txt = `kodam kamu adalah *${res.name}*\n ${res.meaning}`.trim()
+
+    sock.sendMessage(chatId, { text: txt }, { quoted: msg })
     console.log(res)
   } catch (err) {
     sock.sendMessage(chatId, { text: err }, { quoted: msg })
