9e7be6b9910cf11dda29c8ff54dead09a6d8f6a8
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-21 08:31:41 +0700

    fitur hdbsearch

--- src/plugins/toram/hdb.js
+++ src/plugins/toram/hdb.js
@@ -1,21 +1,52 @@
-import { supabase } from "../../model/supabase.js"
+import { supabase } from "../../model/supabase.js";
+
 export const searchHdb = async (sock, chatId, msg, text) => {
   try {
-    const arg = text.replace("!hdb", "");
-    if (!arg) return sock.sendMessage(chatId, { text: "kamu harus tulis nama bos hdb setelah cmd !hdb etoise" }, { quoted: msg })
+    const arg = text.replace("!hdb", "").trim();
 
-    const res = await supabase.from("hdb").select("bosname, stat").ilike("bosname", `%${arg}%`)
+    if (!arg) {
+      return sock.sendMessage(
+        chatId,
+        { text: "kamu harus tulis nama bos hdb setelah cmd !hdb etoise" },
+        { quoted: msg }
+      );
+    }
+
+    const { data, error } = await supabase
+      .from("hdb")
+      .select("bosname, stat")
+      .ilike("bosname", `%${arg}%`)
+      .limit(1);
+
+    if (error) throw error;
+
+    if (!data || data.length === 0) {
+      return sock.sendMessage(
+        chatId,
+        { text: "data hdb tidak ditemukan" },
+        { quoted: msg }
+      );
+    }
+
+    const item = data[0];
 
     const msgtext = `
-    *Hdb stat By Neura Sama*
-    *${res.bosname}*
-    *Stat def:*
-    ${res.stat}
-    `.trim()
+*Hdb stat By Neura Sama*
+*${item.bosname}*
+*Stat def:*
+${item.stat}
+`.trim();
 
-    sock.sendMessage(chatId, { text: msgtext }, { quoted: msg });
-
+    await sock.sendMessage(
+      chatId,
+      { text: msgtext },
+      { quoted: msg }
+    );
   } catch (err) {
-    sock.sendMessage(chatId, { text: err }, { quoted: msg })
+    await sock.sendMessage(
+      chatId,
+      { text: "terjadi kesalahan sistem" },
+      { quoted: msg }
+    );
   }
-}
+};
