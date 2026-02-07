72d4acd05cb3066eb1997e909453f2b0be59ab6e
dimasyoga42 <dimasyoga9090@gmail.com>
2026-02-07 22:25:49 +0700

    fix bug mt

--- src/plugins/vip/tools/btn.js
+++ src/plugins/vip/tools/btn.js
@@ -1,39 +1,34 @@
 import { registerCommand } from "../../../../setting.js"
 
-export const btntest = async (sock, chatId, m) => {
-  const buttons = [
-    {
-      buttonId: ".ping",
-      buttonText: { displayText: "Ping" },
-      type: 1
-    },
-    {
-      buttonId: ".menu",
-      buttonText: { displayText: "Menu" },
-      type: 1
-    },
-    {
-      buttonId: ".owner",
-      buttonText: { displayText: "Owner" },
-      type: 1
-    }
-  ]
-
-  const buttonMessage = {
-    text: "Test Button Neura Sama",
-    footer: "Neura Sama Bot",
-    buttons: buttons,
-    headerType: 1
-  }
-
-  await sock.sendMessage(chatId, buttonMessage, { quoted: m })
+export async function btnTest(sock, chatId, m) {
+  await sock.sendMessage(chatId, {
+    text: "Neura Sama Interactive Button",
+    footer: "Neura Sama",
+    interactiveButtons: [
+      {
+        name: "quick_reply",
+        buttonParamsJson: JSON.stringify({
+          display_text: "Ping",
+          id: ".ping"
+        })
+      },
+      {
+        name: "quick_reply",
+        buttonParamsJson: JSON.stringify({
+          display_text: "Menu",
+          id: ".menu"
+        })
+      }
+    ]
+  }, { quoted: m })
 }
+
 registerCommand({
   name: "btntest",
   alias: ["button"],
   category: "Test",
   desc: "test button message",
   run: async (sock, chatId, msg) => {
-    await btntest(sock, chatId, msg)
+    await btnTest(sock, chatId, msg)
   }
 })
