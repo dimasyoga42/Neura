5066209461afa23c01d4e21de7ce9a3abd79c355
dimasyoga42 <dimasyoga9090@gmail.com>
2026-02-07 15:40:51 +0700

    change prefix

--- src/modul/cmdControls.js
+++ src/modul/cmdControls.js
@@ -457,9 +457,10 @@ export const cmdMenucontrol = async (sock, chatId, msg, text) => {
       Object.keys(grouped).forEach((cat) => {
         menutext += `*${cat}*\n`
         grouped[cat].forEach((cmd) => {
-          const alias = cmd.alias?.length ? ` (${cmd.alias.join(", ")})` : ""
-          menutext += `.${cmd}${alias} - ${cmd.desc}\n`
+          const alias = cmd.alias?.length ? ` (${cmd.alias[0]})` : ""
+          menutext += `.${alias} - ${cmd.desc}\n`
         })
+
         menutext += `\n`
       })
 
