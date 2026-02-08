d97a1c4bbb68ced91cfee67bd7aa97a3e63cfa16
dimasyoga42 <dimasyoga9090@gmail.com>
2026-02-08 07:09:01 +0700

    fix menu

--- src/config/overlay.js
+++ src/config/overlay.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import sharp from "sharp";
 
 export async function addOverlayFromUrl(imageUrl, text) {
   try {
@@ -18,7 +17,7 @@ export async function addOverlayFromUrl(imageUrl, text) {
     const svgOverlay = `
         <svg width="${width}" height="${overlayHeight}">
             <rect x="0" y="0" width="100%" height="100%" fill="rgba(0,0,0,0.6)" />
-            <text  text-anchor="middle" dy=".3em"
+            <text x="10%" y="50%" text-anchor="middle" dy=".3em"
                 fill="white" font-family="Arial" font-size="${Math.floor(overlayHeight * 0.4)}" font-weight="bold">
                 ${text}
             </text>
