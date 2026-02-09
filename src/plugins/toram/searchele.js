efc43ef8b05482fe1b124c609f231cf47b6a3b01
dimasyoga42 <dimasyoga9090@gmail.com>
2026-02-09 21:39:45 +0700

    fix bug admin cmd

--- src/plugins/toram/searchele.js
+++ src/plugins/toram/searchele.js
@@ -15,9 +15,8 @@ export const eleBos = async (sock, chatId, msg, text) => {
     if (!name) return sock.sendMessage(chatId, { text: "tolong masukan element setelah !elebos" }, { quoted: msg });
     const { data, error } = await supabase.from("bosdef").select("name, element").ilike("element", `%${name}%`);
     if (data.length === 0 || error) return sock.sendMessage(chatId, { text: "element yang anda cari tidak ada" }, { quoted: msg });
-    const msgTxt = data.map((item, i) => {
-      return `${i + 1}. ${item.name}\n`
-    })
+    const msgTxt = data.map((item, i) => `${i + 1}. ${item.name}\n`
+    )
     sock.sendMessage(chatId, { text: `Daftar Nama Bos Berdasarkan Element\n${msgTxt}`.trim() }, { quoted: msg })
   } catch (error) {
 
