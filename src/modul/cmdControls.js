99a6e8f8a46f2c9a5a558db4b985c94b224e0e61
dimasyoga42 <dimasyoga9090@gmail.com>
2026-02-09 07:14:34 +0700

    fix bug

--- src/modul/cmdControls.js
+++ src/modul/cmdControls.js
@@ -51,8 +51,6 @@ import { itemStat } from "../plugins/toram/filter.js";
 import { Loli } from "../plugins/fun/loli.js";
 import { commands, fetchdata, message, registerCommand } from "../../setting.js";
 import { addOverlayFromUrl } from "../config/overlay.js";
-import { isAdminvalid } from "../admin/controlAdmin.js";
-import { join, setDesk } from "../plugins/owner/join.js";
 
 export const cmdMenucontrol = async (sock, chatId, msg, text) => {
   // Register all commands
@@ -935,26 +933,5 @@ nomer owner: 085664393331 (dimas)`
       bacaBuku(sock, chatId, msg, text);
     }
   });
-  registerCommand({
-    name: "setdesk",
-    alias: ["setdesk"],
-    category: "Menu Admin",
-    desc: "untuk ganti desc grub",
-    run: async (sock, chatId, msg, args, text) => {
-      if (!isAdminvalid(sock, chatId, msg)) return;
-      if (isBan(sock, chatId, msg)) return;
-      setDesk(sock, chatId, msg, text);
-      sock.sendMessage(chatId, { text: "berhasil di perbarui" }, { quoted: msg });
-    }
-  });
-  registerCommand({
-    name: "join",
-    alias: ["join"],
-    category: "owner",
-    desc: "untuk masuk grub",
-    run: async (sock, chatId, msg, args, text) => {
-      if (!isOwner(sock, msg, chatId)) return;
-      join(sock, chatId, msg, text)
-    }
-  });
+
 };
