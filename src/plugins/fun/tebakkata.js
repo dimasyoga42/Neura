33b315cafb7f13fc87534dea90759726c8f21192
dimasyoga42 <dimasyoga9090@gmail.com>
2026-02-12 23:00:47 +0700

    feat: add tebakkata game plugin with answer tracking and timeout functionality

---
+++ src/plugins/fun/tebakkata.js
@@ -0,0 +1,71 @@
+import { API, registerCommand } from "../../../setting.js";
+import axios from "axios";
+import { isBan } from "../fitur/ban.js";
+
+const answer = new Map()
+
+export const Tekateki = async (sock, chatId, msg, text) => {
+  try {
+    if (answer.has(chatId)) return sock.sendMessage(chatId, { text: "selesaikan permainan yang sedang berjalan" }, { quoted: msg })
+    const res = await axios(API.tebakkata)
+    const getData = res.data
+    //generate soal
+    const key = Math.floor(Math.random() * getData.length) + 1
+    const soal = getData[key]
+    //save Jawaban
+    const timeout = setTimeout(async () => {
+      if (!answer.has(chatId)) return
+      answer.delete(chatId)
+      await sock.sendMessage(
+        chatId,
+        { text: `waktu habis\njawaban: ${soal.jawaban}` },
+        { quoted: sent }
+      )
+    }, 60000)
+    answer.set(chatId, { jawaban: soal.jawaban, timeout })
+    const message = `
+    *${soal.soal}*
+    Category: ${soal.tipe}
+    Time: 60 sec
+    Note: Jawab dengan cara replay bubble chat ini.
+    `.trim()
+    //sock.sendMessage(chatId, {text: re})
+    console.log(answer)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+export const jawabTebakkata = async (sock, chatId, msg) => {
+  try {
+    if (!answer.has(chatId)) return
+
+    const game = answer.get(chatId)
+
+    const userAnswer = msg.message.extendedTextMessage.text.trim().toLowerCase()
+    if (!userAnswer) return
+
+    const correct = game.jawaban.toLowerCase()
+
+    if (userAnswer === correct || correct.includes(userAnswer)) {
+      clearTimeout(game.timeout)
+      answer.delete(chatId)
+      return sock.sendMessage(chatId, { text: "jawaban benar" }, { quoted: msg })
+    }
+    return sock.sendMessage(chatId, { text: "jawaban salah" }, { quoted: msg })
+
+  } catch (error) {
+    sock.sendMessage(chatId, { text: error.message }, { quoted: msg })
+  }
+}
+
+registerCommand({
+  name: "tebakkata",
+  alias: ["mainkata"],
+  category: "Menu fun",
+  desc: "permainan tebak kata",
+  run: async (sock, chatId, msg, args, text) => {
+    if (isBan(sock, chatId, msg)) return
+    Tekateki(sock, chatId, msg, text)
+  }
+})
