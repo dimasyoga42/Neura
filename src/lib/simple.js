3c7f0cefc1e48f29bc140ce02410d575048d2798
dimasyoga42 <dimasyoga9090@gmail.com>
2026-02-12 14:59:43 +0700

    feat: simplify baileys import in simple.js

--- src/lib/simple.js
+++ src/lib/simple.js
@@ -1,12 +1,6 @@
-import pkg from "@whiskeysockets/baileys"
 import fetch from "node-fetch"
 import fs from "fs"
-
-const {
-  proto,
-  generateWAMessageFromContent,
-  generateWAMessageContent
-} = pkg
+import { proto, generateWAMessageFromContent, generateWAMessageContent } from "@whiskeysockets/baileys"
 
 /**
  * ===============================
