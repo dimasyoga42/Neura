1a186c7921a85c4b317364b0130c382f90f880d8
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-05 06:08:52 +0700

    feat: improve error handling and code readability in myBini plugin

--- src/plugins/fun/mybini.js
+++ src/plugins/fun/mybini.js
@@ -5,36 +5,29 @@ export const myBini = async (sock, chatId, msg) => {
     const { data } = await axios.get(
       "https://neuraapi.vercel.app/api/etc/waifu",
     );
-    console.log(data);
-    if (!data || !data.result || data.result.status !== 200) {
+
+    if (data?.result?.status !== 200) {
       return await sock.sendMessage(
         chatId,
-        { text: "gagal mengambil data waifu" },
+        { text: "Gagal mengambil data waifu" },
         { quoted: msg },
       );
     }
 
-    const result = data.result;
-    console.log(result[0].Url);
-
-    const messagetxt = `Bini Kamu Adalah ${result.message}
-Source: ${result.source} - (${result.art})
-Jumlah Image tersedia: ${result.jumlah}`.trim();
+    const { message, source, art, jumlah, Url } = data.result;
 
     await sock.sendMessage(
       chatId,
       {
-        image: { url: result.Url },
-        caption: messagetxt,
+        image: { url: Url },
+        caption: `Bini Kamu: ${message}\nSource: ${source} (${art})\nJumlah: ${jumlah}`,
       },
       { quoted: msg },
     );
-  } catch (err) {
+  } catch {
     await sock.sendMessage(
       chatId,
-      {
-        text: "server error, coba lagi nanti",
-      },
+      { text: "Server error, coba lagi nanti" },
       { quoted: msg },
     );
   }
