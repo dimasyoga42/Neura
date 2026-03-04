c77076f04de368eb1a16f0530f41ae0a4c3b8bb9
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-05 06:01:32 +0700

    feat: enhance myBini plugin with error handling and API timeout

--- src/plugins/fun/mybini.js
+++ src/plugins/fun/mybini.js
@@ -2,15 +2,42 @@ import axios from "axios";
 
 export const myBini = async (sock, chatId, msg) => {
   try {
-    const waifu = await axios.get("https://neuraapi.vercel.app/api/etc/waifu");
-    const data = waifu.data.result;
-    const messagetxt = `Bini Kamu Adalah ${data.message}\n\n source: ${data.source} - (${data.art})\nJumlah Image tersedia: ${data.jumlah}`;
-    sock.sendMessage(
+    const { data } = await axios.get(
+      "https://neuraapi.vercel.app/api/etc/waifu",
+      {
+        timeout: 15000,
+      },
+    );
+
+    if (!data || !data.result || data.result.status !== 200) {
+      return await sock.sendMessage(
+        chatId,
+        { text: "gagal mengambil data waifu" },
+        { quoted: msg },
+      );
+    }
+
+    const result = data.result;
+
+    const messagetxt = `Bini Kamu Adalah ${result.message}
+Source: ${result.source} - (${result.art})
+Jumlah Image tersedia: ${result.jumlah}`.trim();
+
+    await sock.sendMessage(
       chatId,
-      { image: { url: data.url }, caption: messagetxt },
+      {
+        image: { url: result.Url },
+        caption: messagetxt,
+      },
       { quoted: msg },
     );
   } catch (err) {
-    sock.sendMessage(chatId, { text: err }, { quoted: msg });
+    await sock.sendMessage(
+      chatId,
+      {
+        text: "server error, coba lagi nanti",
+      },
+      { quoted: msg },
+    );
   }
 };
