d1a57e497e63c0ed1ec2464a153964b0c33617c2
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-07 22:28:45 +0700

    `fix: handle API response and error in newkhodam function`

--- src/plugins/fun/khodam.js
+++ src/plugins/fun/khodam.js
@@ -2,14 +2,26 @@ import { Resapi } from "../../../setting.js";
 
 export const newkhodam = async (sock, chatId, msg) => {
   try {
-    const data = await fetch(`${Resapi.neura}/etc/khodam`);
-    const res = data.json;
-    const message = res.result;
-    const txt =
-      `khodam kamu adalah ${message.data.khodam} kerena ${message.data.alasan}`.trim();
+    const response = await fetch(`${Resapi.neura}/etc/khodam`);
 
-    sock.sendMessage(chatId, { text: txt }, { quoted: msg });
+    const res = await response.json();
+
+    if (res.result && res.result.data && res.result.data.length > 0) {
+      const item = res.result.data[0];
+
+      const txt =
+        `khodam kamu adalah ${item.khodam} karena ${item.alasan}`.trim();
+
+      await sock.sendMessage(chatId, { text: txt }, { quoted: msg });
+    } else {
+      throw new Error("Format data API tidak valid atau data kosong.");
+    }
   } catch (err) {
-    sock.sendMessage(chatId, { text: err.message }, { quoted: msg });
+    console.error("Error pada fungsi newkhodam:", err);
+    await sock.sendMessage(
+      chatId,
+      { text: `Terjadi kesalahan: ${err.message}` },
+      { quoted: msg },
+    );
   }
 };
