0212b24e96b58f8ce75a27a8d96ad35f85c98149
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-13 02:59:13 +0700

    feat: refactor Loli plugin to use sendImage function for sending loli images

--- src/plugins/fun/loli.js
+++ src/plugins/fun/loli.js
@@ -1,7 +1,15 @@
-export const Loli = (sock, chatId, msg, text) => {
+import { sendImage } from "../../lib/message";
+
+export const Loli = async (sock, chatId, msg, text) => {
   try {
-    sock.sendMessage(chatId, { image: { url: "https://api.deline.web.id/random/loli" } }, { quoted: msg })
+    await sendImage(
+      sock,
+      chatId,
+      "https://api.deline.web.id/random/loli",
+      "ini loli buat kamu",
+      msg,
+    );
   } catch (error) {
-    sock.sendMessage(chatId, { text: error }, { quoted: msg })
+    sock.sendMessage(chatId, { text: error }, { quoted: msg });
   }
-}
+};
