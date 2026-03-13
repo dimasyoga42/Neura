85cda2455521680f51ed3a5d670cfa5be0bd5b42
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-13 15:47:30 +0700

    feat: enhance WhatsApp audio message with metadata and correct mimetype

--- src/plugins/vip/downloader/play.js
+++ src/plugins/vip/downloader/play.js
@@ -39,12 +39,23 @@ export const play = async (sock, chatId, msg, text) => {
     });
 
     // Kirim audio ke WhatsApp
-    await sock.sendMessage(
+    sock.sendMessage(
       chatId,
       {
-        audio: { url: data.data.url },
-        mimetype: "audio/mp4",
-        ptt: false,
+        audio: { url: data.data.url }, // Mengambil langsung dari stream URL API
+        mimetype: "audio/mpeg",
+        fileName: `${data.title}.mp3`,
+        ptt: false, // Set ke true jika ingin dikirim sebagai Voice Note (VN)
+        contextInfo: {
+          externalAdReply: {
+            title: data.title,
+            body: data.channel,
+            thumbnailUrl: data.thumbnail,
+            sourceUrl: data.data.url,
+            mediaType: 2, // Menandakan tipe media audio
+            showAdAttribution: true,
+          },
+        },
       },
       { quoted: msg },
     );
