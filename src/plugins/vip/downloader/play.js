7cee1adc6f5195d9011a465b61a8cdce48c571a3
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-13 14:58:51 +0700

    `feat: enhance play functionality with improved error handling and music metadata display`

--- src/plugins/vip/downloader/play.js
+++ src/plugins/vip/downloader/play.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import fetch from "node-fetch";
 import { sendAudio, sendFancyText } from "../../../lib/message.js";
 
 export const play = async (sock, chatId, msg, text) => {
   try {
     const query = text.replace(".play", "").trim();
+
     if (!query) {
       return await sock.sendMessage(
         chatId,
@@ -12,27 +12,34 @@ export const play = async (sock, chatId, msg, text) => {
         { quoted: msg },
       );
     }
+
     const res = await axios.get(
       `https://api.neoxr.eu/api/play?q=${encodeURIComponent(query)}&apikey=${process.env.NOXER}`,
     );
-    if (!data.data)
-      return sendFancyText(sock, chatId, {
+
+    const data = res.data;
+
+    if (!data?.data?.url) {
+      return await sendFancyText(sock, chatId, {
         title: "Neura Play",
         body: "musik yang anda cari tidak ada",
         text: "",
         thumbnail:
           "https://i.pinimg.com/1200x/58/64/04/58640492bafe2aa0d98c00c2b326448b.jpg",
-        msg: msg,
+        quoted: msg,
       });
-    const data = res.data;
-    sendFancyText(sock, chatId, {
+    }
+
+    await sendFancyText(sock, chatId, {
       title: "Neura Play",
-      body: `music: ${data.title}`,
+      body: `Music: ${data.title}`,
+      text: `Channel: ${data.channel}\nDurasi: ${data.fduration}`,
       thumbnail: data.thumbnail,
-      msg: msg,
+      quoted: msg,
     });
-    sendAudio(sock, chatId, data.data.url, true, msg);
+
+    await sendAudio(sock, chatId, data.data.url, true, msg);
   } catch (err) {
-    sock.sendMessage(chatId, { text: err.message }, { quoted: msg });
+    await sock.sendMessage(chatId, { text: err.message }, { quoted: msg });
   }
 };
